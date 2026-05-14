114e4c13438ea38bfd06c409a8a434f6a3ad2e2f
pabloriospena <pabloriospena@gmail.com>
2026-05-14 11:33:49 -0500

    Commit inicial: Base Astro + Clerk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import clerk from '@clerk/astro';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [clerk()],
+  vite: {
+    css: {
+      postcss: './postcss.config.mjs',
+    }
+  }
+});
