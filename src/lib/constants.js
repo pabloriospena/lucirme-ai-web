b91b579423338ee93cd4564a3b8413d70f0138cc
pabloriospena <pabloriospena@gmail.com>
2026-05-27 08:49:02 -0500

    New: Calendar with Cal

---
+++ src/lib/constants.js
@@ -0,0 +1,7 @@
+// src/lib/constants.js
+export const LINKS = {
+  calendly: "https://cal.com/pablolucirme",
+  ebook: "https://pay.hotmart.com/D96093022S?off=OFF50",
+  email: "mailto:pabloriospena@gmail.com",
+  linkedin: "https://linkedin.com/in/pabloriospena"
+};
