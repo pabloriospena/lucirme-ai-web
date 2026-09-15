8298cb428162e7d9fd9691b05d74dd485cc8bdbb
pabloriospena <pabloriospena@gmail.com>
2026-09-15 18:18:48 -0500

    Nuevo producto 18 prompts

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -14,5 +14,6 @@ export const LINKS = {
   calendly_empresa: "https://cal.com/pablolucirme/diagnostico-gratuito",
   calendly_taller: "https://cal.com/pablolucirme/taller-de-inmersion",
   bot: "https://pay.hotmart.com/F107452795B",
+  atajos_ia: "https://pay.hotmart.com/X107628052G",
   whatsapp: "https://wa.me/573053046180",
 };
